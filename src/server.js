81e0bbb3bcf8cfef335f08264e5cff2fcf4f034d
Pashchenko Volodymyr <pikimelposhta@gmail.com>
2024-08-12 12:10:31 +0300

    init

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import request from 'requests';
+import queryString from 'querystring';
+import cors from 'cors';
+const app = express();
+
+app.use(cors());
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+import { env } from './env.js';
+
+export const SPOTIFY_CLIENT = env('SPOTIFY_CLIENT');
+export const SPOTIFY_SECRET = env('SPOTIFY_SECRET');
+export const APPLE_MUSIC_SECRET = env('APPLE_MUSIC_SECRET');
+export const REDIRECT_URL = env('REDIRECT_URL');
